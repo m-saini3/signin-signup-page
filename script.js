6c1c06c608feafcf502d72d3eca389c2fea0e177
Manish Saini <78289491+STR3AK03@users.noreply.github.com>
2023-08-26 07:00:45 +0530

    files updated

--- script.js
+++ script.js
@@ -1,16 +1,6 @@
 const password = document.querySelector(".password");
 const passwordBtn = document.querySelector(".password-btn");
 
-// function togglePassword() {
-//   hello.style.color = "red";
-//   password.type = "text";
-// }
-
-// const togglePassword = () => {
-//   hello.style.color = "red";
-//   password.type = "text";
-// };
-
 passwordBtn.addEventListener("click", () => {
   if (password.type == "password") {
     password.type = "text";
